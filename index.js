89f6e99d6948c94bfe7ad3c20d842288375f216c
Hector Ayala <ayabarhec@gmail.com>
2021-03-23 17:07:39 -0600

    Add 2 lists goodMusic and badMusic

---
+++ index.js
@@ -0,0 +1,5 @@
+/* So this is our main code to formulate the program, first we will need to create 3 data bases
+each one with unique phrases but in a way that can compliment each other, so we can make some way to
+randomize them and print them out in an ordered way to the user. 
+*/
+
