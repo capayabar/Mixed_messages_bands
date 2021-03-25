482234421f63421f7075b3bcab27e6d1ffff9313
Hector Ayala <ayabarhec@gmail.com>
2021-03-25 09:22:39 -0600

    Rework the functions, variables & opening statement

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-/* So this is our main code to formulate the program, first we will need to create 3 data bases
-each one with unique phrases but in a way that can compliment each other, so we can make some way to
-randomize them and print them out in an ordered way to the user. 
+/* So this is our main code to the program, it has an object and an array as its sources,
+then by a function it chooses first if it will be a warning or recomendation, then if it's a warning
+it chooses an artist from the array, if it's a recomendation it chooses an artist from the object and
+from this artist albums nested array, and finally all its logged.
 */
 
-// first list to choose from, this will be chosen by a randomer.
+// first list to choose from, this will be chosen by a randomizer.
 const goodMusic = {
     bandOrArtist: {
         'Guns n Roses': {
@@ -29,20 +30,33 @@ const badMusic = ['Nickelback', 'Creed', 'The Pussycat Dolls', 'Milli Vanilli', 
 
 // function to choose which message should appear and which options to choose
 const musicSelector = () => {
-    let bandName = [];
-    let albumName = [];
-    let randomBadBand = Math.floor(Math.random() * badMusic.length);
-    let goodOrBad = Math.floor(Math.random() * 2);
+    
+    let bandName = []; //Array for all the band types in the goodMusic object
+    
+    let albumName = []; //Array for the number of albums there are in the almbum key of the band name
+    
+    let goodOrBad = Math.floor(Math.random() * 2); // this one decides if it should give you a good or bad band or artist
+    
     for (artist in goodMusic.bandOrArtist) {
         bandName.push(artist);
-        console.log(bandName);
-    }
+    }; // this function adds the artists to the bandName array.
+    
+    let randomBadBand = Math.floor(Math.random() * badMusic.length); //this is a randomizer to choose the bad recomendation
+    
+    let randomGoodBand = Math.floor(Math.random() * bandName.length);  //this randomizer for choosing the cand album
+    
+    for (album in goodMusic.bandOrArtist[bandName[randomGoodBand]].albums) {
+        albumName.push(album);
+    }; // in this function we push the albums to the albumName array.
+    
+    let randomAlbum = Math.floor(Math.random() * albumName.length); //we use this randomizer to choose the album name.
+      
     
     if (goodOrBad === 0) {
-        console.log(`You should never listen to ${badMusic[randomBadBand]}`);
-    } 
-    let randomArtist = [];
-    let randomAlbum = [];
+        console.log(`You should never listen to ${badMusic[randomBadBand]}`); 
+    } else {
+        console.log(`You should listen to ${bandName[randomGoodBand]} and the album ${goodMusic.bandOrArtist[bandName[randomGoodBand]].albums[randomAlbum]}`);
+    }; //This conditional gives us the recomendation, it's a little lengthy but it works.
 }
 
 musicSelector();
